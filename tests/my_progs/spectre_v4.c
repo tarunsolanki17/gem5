e422b9816a3297b1aeb5cf2932150860143f2122
Tarun Solanki <tarunsolanki97@gmail.com>
2020-04-08 15:19:11 +0530

    Spectre v4 Try2
    Change-Id: I398cbbc52682681baf8bcd9ba49c1a81fb593e9a

--- tests/my_progs/spectre_v4.c
+++ tests/my_progs/spectre_v4.c
@@ -10,15 +10,41 @@ double q = 0;
 char array[256 * 4 * 1024];
 int x;
 
-int main(){
-    
-    
-    //TODO: Check if speculation did occur or not. Look in the gem5 stats if the load to y was correct or not.
+char K[8196];
+char M[8196];
+char *X;
+char y;
 
-    char *array_ptr = array;
-    array[0] = 2;
+int main(){    
 
-    q = ch;
+    int i;
+
+    m5_global_init();
+    _mm_lfence();
+    _mm_mfence();
+
+    for(i=0;i<=1023;i++)
+        K[i] = 0x1;
+    
+    X = K;
+
+    for(i=0;i<=1023;i++)
+        M[i] = 0x5;
+
+    X = M;
+    y = *X;
+
+    _mm_lfence();
+    _mm_mfence();
+    m5_global_init();
+
+
+    // char *array_ptr = array;
+    // array[0] = 2;
+
+    // q = ch;
+
+
 
     // _mm_clflush(array);
 
@@ -36,7 +62,7 @@ int main(){
 
     // "movq $array, %rdi;"
 
-    m5_global_init();
+    // m5_global_init();
     _mm_lfence();
     _mm_mfence();
 
@@ -53,19 +79,15 @@ int main(){
                 "movq %r8, (%rax);"                // A memory write which will be placed in the store buffer. 
                                                   // You need to reaccess this and get the stale value instead, due to the speculative execution.
                 "mov (%rax), %ebx;"
-                "imul $64, %rbx, %rbx;"
+                "imul $64, %ebx, %ebx;"
                 
                 "add %rbx, %rcx;"
-                "mov (%rcx), %dl;"
-                
-
-
-                
+                "mov (%rcx), %dl;"                  // This request to memory must be from 0x6bc3c0 (for success of the attack)                       
     );
                 
     _mm_lfence();
     _mm_mfence();
-    m5_global_init();
+    // m5_global_init();
 
     printf("array[] = %d \n", array[0]);
 
